63a67acb3445c27445e0e11cc2df1b54c8c630bd
Tamar Bem Nahum <105340024+TamarBeNahum@users.noreply.github.com>
2024-03-29 16:07:32 +0300

    hh

---
+++ InternViewServer/Models/Intern.cs
@@ -0,0 +1,15 @@
+﻿namespace InternViewServer.Models
+{
+    public class Intern
+    {
+        public int id { get; set; }
+        public string First_name { get; set; }
+
+        public string Last_name { get; set; }
+
+        public string Interns_year { get; set; }
+
+        public int Interns_rating { get; set; }
+
+    }
+}
